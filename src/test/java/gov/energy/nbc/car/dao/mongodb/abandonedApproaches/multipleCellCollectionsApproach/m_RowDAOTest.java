c67064aaec804702fad734eae6e86268815a71d7
mbrown <mike.brown@nerel.gov>
2016-01-02 09:51:34 -0700

    Created abandonedApproaches package for the bo and dao classes that represent approaches that were abandoned

---
+++ src/test/java/gov/energy/nbc/car/dao/mongodb/abandonedApproaches/multipleCellCollectionsApproach/m_RowDAOTest.java
@@ -0,0 +1,15 @@
+package gov.energy.nbc.car.dao.mongodb.abandonedApproaches.multipleCellCollectionsApproach;
+
+import gov.energy.nbc.car.app.DataRepositoryApplication;
+import gov.energy.nbc.car.bo.mongodb.abandonedApproaches.multipleCellSchemaApproach.m_BusinessObjects;
+import gov.energy.nbc.car.dao.mongodb.AbsRowDAOTest;
+import gov.energy.nbc.car.settings.Settings;
+
+public class m_RowDAOTest extends AbsRowDAOTest {
+
+    @Override
+    protected DataRepositoryApplication createAppSingleton(Settings settings) {
+
+        return new DataRepositoryApplication(settings, new m_BusinessObjects(settings));
+    }
+}
