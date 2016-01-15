d8301f04b6e2f9d05e573f6a5bddd6e685f2f591
mbrown <mike.brown@nerel.gov>
2016-01-15 11:07:16 -0700

    changes to unit testing code

--- src/test/java/gov/energy/nrel/dataRepositoryApp/dao/mongodb/TestUsingTestData.java
+++ src/test/java/gov/energy/nrel/dataRepositoryApp/dao/mongodb/TestUsingTestData.java
@@ -7,10 +7,20 @@ import gov.energy.nrel.dataRepositoryApp.dao.exception.CompletelyFailedToPersist
 import gov.energy.nrel.dataRepositoryApp.dao.exception.PartiallyFailedToPersistDataset;
 import gov.energy.nrel.dataRepositoryApp.settings.Settings;
 
+import java.util.UUID;
+
 public abstract class TestUsingTestData {
 
     static public boolean SUSPEND_DATA_SEEDING = false;
     static public boolean SUSPEND_DATA_CLEANUP = false;
+
+    static final public String UUID_TO_AVOID_COLLISSIONS_IF_MORE_THAN_ONE_CLIENT_IS_RUNNING_UNIT_TESTS;
+    static {
+        String string = UUID.randomUUID().toString();
+        int length = string.length();
+        UUID_TO_AVOID_COLLISSIONS_IF_MORE_THAN_ONE_CLIENT_IS_RUNNING_UNIT_TESTS =
+                string.substring(length - 4, length);
+    }
 
     public static final String[] DEFAULT_SET_OF_DATA_CATEGORIES = new String[] {"Algae", "ATP3", "Biomass"};
 
@@ -34,7 +44,9 @@ public abstract class TestUsingTestData {
 
         settings.setMongoDbHost("localhost");
         settings.setMongoDbPort("27017");
-        settings.setMongoDatabaseName("data-repository-app_UNIT_TESTING");
+        settings.setMongoDatabaseName(
+                "data-repository-app_UNIT_TESTING_" +
+                UUID_TO_AVOID_COLLISSIONS_IF_MORE_THAN_ONE_CLIENT_IS_RUNNING_UNIT_TESTS);
         settings.setRootDirectoryForUploadedDataFiles("target/test-classes/testData");
         settings.setDefaultSetOfDataCategories(DEFAULT_SET_OF_DATA_CATEGORIES);
         settings.setPerformanceLoggingEnabled(false);
